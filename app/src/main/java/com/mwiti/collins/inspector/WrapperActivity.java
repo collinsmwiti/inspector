903b207b3edcb7ff9b00df0c32381ecc036fb2aa
Collins Mwiti <mwiticmutumamwenda@gmail.com>
2017-08-19 14:07:43 +0300

    created activity wrapper to act as a common class in order to set up boilerplate logic(abstract class) for the app

---
+++ app/src/main/java/com/mwiti/collins/inspector/WrapperActivity.java
@@ -0,0 +1,13 @@
+package com.mwiti.collins.inspector;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WrapperActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wrapper);
+    }
+}
