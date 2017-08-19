407d3a8cc59d4f45da0905d75b8a4def4935fa6c
Collins Mwiti <mwiticmutumamwenda@gmail.com>
2017-08-19 12:55:13 +0300

    added uses permission in the app at manifest file

--- app/src/main/java/com/mwiti/collins/inspector/MainActivity.java
+++ app/src/main/java/com/mwiti/collins/inspector/MainActivity.java
@@ -1,13 +1,21 @@
 package com.mwiti.collins.inspector;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
+
+//class main activity
 public class MainActivity extends AppCompatActivity {
 
+    /**
+     * On create.
+     *
+     * @param savedInstanceState the saved instance state
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
